37b556a1f3bc6de2c992c54353811d881b40af78
Kotwade <kotwade@yandex.ru>
2021-05-07 13:29:48 +0500

    Добавляет начало js

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,71 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var ESC_KEY = 'Escape';
 
-pageHeader.classList.remove('page-header--nojs');
+var popup = document.querySelector('.popup');
+var popupOpen = document.querySelector('.contacts-header__button');
+var popupClose = popup.querySelector('.popup__button-cross');
+var overlay = document.querySelector('.overlay');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+var onPopupEscPress = function (evt) {
+  if (evt.key === ESC_KEY) {
+    closePopup();
   }
+};
+
+var openPopup = function () {
+  popup.classList.remove('popup--hidden');
+  overlay.classList.remove('overlay--hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  popup.classList.add('popup--hidden');
+  overlay.classList.add('overlay--hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+popupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+popupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+overlay.addEventListener('click', function () {
+  popup.classList.add('popup--hidden');
+  overlay.classList.add('overlay--hidden');
+});
+
+// Аккордеон
+
+var accordionItems = document.querySelectorAll('.accordion');
+var accordionPanes = document.querySelectorAll('.accordion__pane');
+
+var hidePane = function (button, pane) {
+  button.classList.add('accordion__toggle--inactive');
+  pane.classList.add('accordion__pane--hidden');
+};
+
+var showPane = function (button, pane) {
+  button.classList.remove('accordion__toggle--inactive');
+  pane.classList.remove('accordion__pane--hidden');
+};
+
+var toggleAccordion = function (evt) {
+  Array.prototype.forEach.call(accordionPanes, function (accordionPane) {
+    var button = accordionPane.closest('.accordion').querySelector('.accordion__toggle');
+    if (button === evt.target && !button.classList.contains('accordion__toggle--inactive') || button !== evt.target) {
+      hidePane(button, accordionPane);
+    } else if (button === evt.target) {
+      showPane(button, accordionPane);
+    }
+  });
+};
+
+Array.prototype.forEach.call(accordionItems, function (accordion) {
+  var accordionToggleButton = accordion.querySelector('.accordion__toggle');
+  var accordionPane = accordion.querySelector('.accordion__pane');
+  hidePane(accordionToggleButton, accordionPane);
+  accordionToggleButton.addEventListener('click', toggleAccordion);
 });
